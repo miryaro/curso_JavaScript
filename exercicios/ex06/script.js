377c8cbd0706a3b5ace2e84a4cd71dda4811725a
Miryã <miryarodrigues11@gmail.com>
2023-07-21 09:47:00 -0300

    exerciciosJS

--- exercicios/ex06/script.js
+++ exercicios/ex06/script.js
@@ -23,9 +23,37 @@ function adicionar() {
         var item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista')
     }
     num.value = ''
     num.focus()
+ }
+ function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicionar valores antes de finalizar!')
+    } else{
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        //vai verificar um numero de cada vez para ver qual é o maior e o menor.
+        // o laço de repetição varre o vetor inteiro para verificação.
+        for(var pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+    }
  }
